f201c17f0f05a55b2c047ce1d052edea82a9a7ec
AlphaCap <info@alphacapturetechnology.com>
2025-07-04 15:04:17 -0700

    Update data_scheduler.py

--- data_scheduler.py
+++ data_scheduler.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the current directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from data_fetcher import DataFetcher
 import time
 import logging
