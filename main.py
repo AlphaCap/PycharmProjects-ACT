91e6711b8329db96cd1b36bcacc338a90227b973
AlphaCap <info@alphacapturetechnology.com>
2025-07-05 07:16:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -533,6 +533,6 @@ if not polygon_api_key and (args.update or args.symbol or args.scheduler):
     
     return 0
 
-if __name__ == "__main__":
+    if __name__ == "__main__":
     print(f"nGS Trading System v{VERSION} - {datetime.now().strftime('%Y-%m-%d')}")
     sys.exit(main())
