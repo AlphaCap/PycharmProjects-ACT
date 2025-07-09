412c4b0b54563f2cd8a1ad55e62b93bd2630f07a
AlphaCap <info@alphacapturetechnology.com>
2025-07-08 21:28:04 -0700

    Update web_dashboard.py

--- web_dashboard.py
+++ web_dashboard.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from ngs_dashboard import create_ngs_dashboard
+from simple_ngs_dashboard import create_simple_dashboard
 
-st.set_page_config(page_title="nGS Live Dashboard", layout="wide")
-
-# Run the complete dashboard system
-create_ngs_dashboard()
+st.set_page_config(page_title="nGS Dashboard", layout="wide")
+create_simple_dashboard()
