32e06b0a2d84f94c1c69cb19ef1ccdd5bd395704
AlphaCap <info@alphacapturetechnology.com>
2025-08-12 09:47:27 -0700

    Delete backup directory

--- backup/utils/config.py
+++
@@ -1,8 +0,0 @@
-# Polygon.io API credentials
-POLYGON_API_KEY = "yTZVrttxzFCK58_gOUGGATWxQzytgAxy"  # API key in quotes
-
-# Default settings
-DAILY_HISTORY_DAYS = 180  # 6 months
-# MINUTE_HISTORY_DAYS = 0   # 5 days
-
-
